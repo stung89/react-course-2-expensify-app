fd91153cf21e1925f950d38b7d54b8a671fda373
stung89 <stung89@gmail.com>
2018-01-16 21:51:18 -0800

    setup dev devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path:path.join(__dirname, 'public'),
+            path:path.join(__dirname, 'public', 'dist'),
             filename:'bundle.js'
         },
         module: {
@@ -44,7 +44,8 @@ module.exports = (env) => {
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             // allows the usage of exact route setup
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     }
 };
