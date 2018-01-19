bf61b5168dfe3bb5a25ca35a2ad480c476fc0459
stung89 <stung89@gmail.com>
2018-01-18 22:01:25 -0800

    Add expense summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,14 @@
+export default (expenses) => {
+    if(expenses.length === 0) {
+        return 0;
+    } else {
+        return expenses
+            .map((expense) => expense.amount)
+            .reduce((sum, value) => sum + value, 0);
+        // return expenses.map((expense) => {
+        //     return expense.amount
+        // }).reduce((sum, value) => {
+        //     return sum + value;
+        // }, 0)
+    }
+}
